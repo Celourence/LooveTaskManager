f9019e90fcc8a86d881e710327a9afcebad99b42
Christian Eduado Lourenço Moreira Santos <43624903+Celourence@users.noreply.github.com>
2025-04-06 19:14:16 -0300

    Update TaskController.cs.cs

--- LooveTaskManager.API/Controllers/TaskController.cs.cs
+++ LooveTaskManager.API/Controllers/TaskController.cs.cs
@@ -4,29 +4,18 @@ namespace LooveTaskManager.API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class TaskController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private readonly ILogger<TaskController> _logger;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public TaskController(ILogger<TaskController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "GetTasks")]
+    public IEnumerable<TaskController> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return new List<TaskController>();
     }
 }
